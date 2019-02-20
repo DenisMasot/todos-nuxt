f01da783b5c19f13ec261f6810b4b3bf7d45b689
DenisMasot <denis.masot@gmail.com>
2019-02-20 11:53:14 +0100

    [todo] first commit add input and function addTodo

---
+++ pages/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        Todos with nuxt
+      </h1>
+
+      <input type="text" placeholder="écrire une nouvelle tache" v-model='newTodo' @keyup.enter="addTodo">
+
+      <ul v-for='todo in todos' :key="todo.index">
+        <li>{{todo.name}}</li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  },
+  data () {
+    return {
+      newTodo: '',
+      todos: [{
+        name: 'tache n°1',
+        completed: false
+      }]
+    }
+  },
+  methods: {
+    addTodo () {
+      this.todos.push({
+        name: this.newTodo,
+        completed: false
+      })
+      this.newTodo = ''
+    }
+  }
+}
+
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
